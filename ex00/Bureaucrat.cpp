881ae75a70f6c706df5a4b114845745b24ff0b29
Mohammed-Abed Hassani-Idrissi <mhassani@c3r3p6.1337.ma>
2023-12-02 19:52:26 +0100

    ex00 done

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,88 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(): _name("Abed")
+{
+    _grade = 150; // lowest grade;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int grade): _name(name)
+{
+    if(grade > 150)
+    {
+        GradeTooLowException low;
+        throw(low);
+    }
+    if(grade < 1)
+    {
+        GradeTooHighException high;
+        throw(high);
+    }
+    _grade = grade;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other): _name(other._name)
+{
+    std::cout << "copy constructor\n";
+    *this = other;
+}
+
+Bureaucrat &Bureaucrat::operator=(const Bureaucrat &other)
+{
+    std::cout << "copy assignement operator\n";
+    if (this != &other)
+    {
+        this->_grade = other._grade;
+    }
+    return *this;
+}
+
+std::string Bureaucrat::getName() const
+{
+    return _name;
+}
+
+int Bureaucrat::getGrade() const
+{
+    return _grade;
+}
+
+const char *Bureaucrat::GradeTooHighException::what() const throw()
+{
+    return "Grade too high\n";
+}
+
+const char *Bureaucrat::GradeTooLowException::what() const throw()
+{
+    return "Grade too low\n";
+}
+
+void Bureaucrat::incrementGrade()
+{
+    _grade--;
+    if(_grade < 1)
+    {
+        GradeTooHighException high;
+        throw(high);
+    }
+}
+
+void Bureaucrat::decrementGrade()
+{
+    _grade++;
+    if(_grade > 150)
+    {
+        GradeTooLowException low;
+        throw(low);
+    }
+}
+
+std::ostream &operator<<(std::ostream &output, Bureaucrat &obj)
+{
+    output << obj.getName() << "\n";
+    return output;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+    std::cout << "Destructor called\n";
+}
---
+++ ex00/main.cpp
@@ -0,0 +1,43 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    try
+    {
+        Bureaucrat a("abed", 151);
+    }
+    catch(std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << "continue1\n"; // this message will displayed in both cases;
+    Bureaucrat b("abed", 150);
+    try
+    {
+        b.decrementGrade();
+    }
+    catch(std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << "continue2\n";
+    Bureaucrat c("abed", 1);
+    try
+    {
+        c.incrementGrade();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << e.what() << std::endl;
+    }
+    std::cout << "continue3\n";
+    try
+    {
+        c.decrementGrade();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    std::cout << "end\n";
+}
